4c43d21fc7574dc9f06e6da1154e63a0112bb28a
kadehill <khill@fullsail.edu>
2015-08-18 19:37:40 -0400

    Defining the Variables

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+//Kade Hill
+//8/18/15
+//SDI 1508 - Section 01
+//Conditionals Assignment
+
+
+//We will be calculating calorie intake using the formula for BMR (basal metabolic rate)
+//BMR Men = 66.47 + (13.75 x W) + (5.0 x H) - (6.75 x A)
+//BMR Women = 665.09 + (9.56 x W) + (1.84 x H) - 4.67 x A)
+//Variables
+
+var weight;
+var height;
+var age;
+var male = true;
+
+
+
