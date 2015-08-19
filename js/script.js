daf9f41f13bed6c56aec43ced36bd55ea3507067
kadehill <khill@fullsail.edu>
2015-08-18 21:52:01 -0400

    Outputs1

--- js/script.js
+++ js/script.js
@@ -8,16 +8,20 @@
 //We will be calculating if the user is reaching their exercise goals per week.
 
 //Variables
-
-
 var totalHoursPerWeek = 168; //total hours in a week
+var totalHoursPerDay = 24;
 var days;        //The number of days per week user exercises
 var hoursPerDay; //The number of hours per day user spends exercising
 var personalGoal; //The user's personal exercise goal
 
 //Prompts
-hoursPerDay = prompt("We are calculating if you've reached your fitness goal. \n Enter how many hours you exercise per day.")
-days = prompt("Enter how many days you exercise per week.")
-personalgoal = prompt("How many hours per week do you strive to execise?")
+personalGoal = prompt("We are calculating if you've reached your fitness goal. \nHow many hours per week do you strive to exercise?");
+hoursPerDay = prompt("Enter how many hours you exercise per day.");
+days = prompt("How many days do you exercise per week?");
 
+
+var total = Number(personalGoal) - Number(hoursPerDay) * Number(days);
+console.log(total);
+
+//outputs
 
