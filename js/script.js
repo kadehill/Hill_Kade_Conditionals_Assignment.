c1e5b7e2db032d5e97f65547525f7c7181c646a9
kadehill <khill@fullsail.edu>
2015-08-19 22:31:32 -0400

    Conditionals

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@
 //We will be calculating if the user is reaching their exercise goals per week.
 
 //Variables
-var days;        //The number of days per week user exercises
-var hoursPerDay; //The number of hours per day user spends exercising
-var personalGoal; //The user's personal exercise goal
-var total;
+var days;        //The number of days per week user exercises.
+var hoursPerDay; //The number of hours per day user spends exercising.
+var personalGoal; //The user's personal exercise goal.
+var total; //The number of hours that the user is short of reaching the goal.
+var cheatWeek = true;
 
 //Prompts
 personalGoal = prompt("We are calculating if you've reached your fitness goal. \nHow many hours per week do you strive to exercise?");
@@ -20,14 +21,13 @@ days = prompt("How many days do you exercise per week?");
 
 //Equation determines how many hours short user is from reaching their goal.
 total = Number(personalGoal) - Number(hoursPerDay) * Number(days);
-console.log("You are" + " " +total + " " + "hours short of reaching your goal");
+console.log("You are" + " " +total + " " + "hours short of reaching your weekly goal.");
 console.log("Your goal was" + " "+ personalGoal + " " + "hours per week.");
 
 //outputs
 if(personalGoal > total && total == 0){
-    console.log("You reached your goal!");
+    console.log("Congratulations! You reached your goal!");
 }else{
     console.log("Sorry, you did not reach your goal.");
 }
-
 
